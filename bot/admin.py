44ab87abefbf7608cea9d6ef3ea00109f7229306
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2021-11-05 19:08:35 +0200

    admin.py: Vacancy, Candidate, Resume added

--- bot/admin.py
+++ bot/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Vacancy
+from .models import Vacancy, Candidate, Resume
 
 admin.site.register(Vacancy)
+admin.site.register(Candidate)
+admin.site.register(Resume)
