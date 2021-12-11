b30fe924888e1511811855f92edc96db0c5003ff
Ann Martynenko <ann.martynenko@true-item.com>
2021-12-11 20:59:21 +0200

    Revert "changed name of requirements model"
    This reverts commit 67f222d27664530dcc503e372add546664b77340.

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Vacancy, Candidate, Resume, Additional_info, Skills, Required_skills
+from .models import Vacancy, Candidate, Resume, Requirements, Skills, Required_skills
 
 admin.site.register(Vacancy)
 admin.site.register(Candidate)
 admin.site.register(Resume)
-admin.site.register(Additional_info)
+admin.site.register(Requirements)
 admin.site.register(Skills)
 admin.site.register(Required_skills)
